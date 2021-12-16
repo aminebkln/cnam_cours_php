41d235bc3e4f5ae0619e74a82372cad05b126fd4
bouakline <mohamed.bouakline@hotmail.com>
2021-12-16 16:58:01 +0100

    ajouté de code

---
+++ conditions.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+
+require __DIR__.'/vendor/autoload.php';
+
+
+
+
+$isWateroff = (bool) random_int(0, 1);
+$isGasoff = (bool) random_int(0,1);
+dump ($isWateroff);
+dump ($isGasoff);
+if ($isWateroff && $isGasoff) {
+     
+    echo 'on puet partir en we! <br>';
+} else {
+
+    echo 'on ne peut pas partie en we<br>';
+    
+    
+
+
+
+
+
+
+
+
+
+
+}
