1b3fa98c8fa88399e46ed7b75e888506ad500ed7
bouakline <mohamed.bouakline@hotmail.com>
2021-12-16 11:44:26 +0100

    Ameliorations du code
    - ajout de code sue les variables
    - ajoute de code dans le index.php

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>document</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>page d'accueil</title>
+    <link rel="stylesheet" href="cours.css">
+   
 </head>
 <body>
-   <h1> page d'accueil</h1> 
+   <h1>titre pricipal </h1> 
    <?php
-   echo'du php dans du Html <br>';
+         $prenom = "pierre";
+         $age = 28;
+   echo'<h1>du php dans du Html <br></h1>';
+   
    echo'(En realité il ne faut surtout pas le faire .)<br>';
+         $prenom = "pierre";
+         $age =28;
+   echo'bonjour je m\'appelle <br>' . $prenom;
+   
+
    ?>
    
 
---
+++ variable.php
@@ -0,0 +1,20 @@
+<?php
+
+$prenom = "mohamed<br>";
+echo $prenom;
+$isMorning = true;
+$issunny =false;
+echo $isMorning ;
+echo '<br>';
+echo $issunny;
+echo '<br>';
+$pi = 3.14;
+$e = 2.71;
+echo $pi;
+echo '<br>';
+echo $e;
+echo '<br>';
+
+$referundum = null;
+echo $referundum;
+echo '<br>';
