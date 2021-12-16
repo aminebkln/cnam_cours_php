b7a41041ad637111f5a37ed0aed13285985a5863
bouakline <mohamed.bouakline@hotmail.com>
2021-12-16 12:39:47 +0100

    Ajout de code
    - Création d'un code sur les chaines de cractéres

---
+++ string.php
@@ -0,0 +1,10 @@
+<?php
+
+$text1 = 'foo';
+$text2 = $text1 . ' bar ';
+
+echo $text1 .'<br>';
+echo $text2. '<br>';
+$temperature = 10.5;
+$report = "la temperature est de $temperature °C";
+echo $report ."<br>\n";
