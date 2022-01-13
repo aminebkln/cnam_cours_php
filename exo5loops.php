8e745bbeefeaed2a5796aa1e6a8c2eac612ab48b
bouakline <mohamed.bouakline@hotmail.com>
2022-01-13 15:24:23 +0100

    exercice 5

---
+++ exo5loops.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+for($i= 0;$i <=9;$i+=1){
+
+    echo "$i<br>";
+
+    if($i==0){
+        echo "premier interation<br>"; 
+    }
+
+    if($i==9){
+        echo "dernier interation<br>";
+    }
+}
+
+
+
